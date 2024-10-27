92df36524354b25761621e3a934593918d1f917c
daniel <danielmayowataiwo@gmail.com>
2024-10-27 01:38:47 +0000

    chore: implement orders email consumer

--- src/consumers/email.consumer.ts
+++ src/consumers/email.consumer.ts
@@ -4,7 +4,10 @@ import {
   winstonLogger,
 } from "@notifications/config";
 import { sendEmail } from "@notifications/emails/mail.transport";
-import { EmailLocals } from "@notifications/models/notifications.model";
+import {
+  EmailLocals,
+  SendEmailDTO,
+} from "@notifications/models/notifications.model";
 import { Channel, ConsumeMessage } from "amqplib";
 import { Logger } from "winston";
 
@@ -88,10 +91,52 @@ export async function consumeOrderEmailMessages(
     channel.consume(
       servicesconnectQueue.queue,
       async (msg: ConsumeMessage | null) => {
-        console.log(JSON.parse(msg!.content.toString()));
-
-        // send emails
-        // acknowledge
+        const dto = JSON.parse(msg!.content.toString()) as SendEmailDTO;
+        const locals: EmailLocals = {
+          app_link: `${envConfig.client_url}`,
+          app_icon: "https://i.ibb.co/Kyp2m0t/cover.png",
+          username: dto.locals.username,
+          sender: dto.locals.sender,
+          offer_link: dto.locals.offer_link,
+          amount: dto.locals.amount,
+          buyer_username: dto.locals.buyer_username,
+          seller_username: dto.locals.seller_username,
+          title: dto.locals.title,
+          description: dto.locals.description,
+          delivery_days: dto.locals.delivery_days,
+          order_id: dto.locals.order_id,
+          order_due: dto.locals.order_due,
+          requirements: dto.locals.requirements,
+          order_url: dto.locals.order_url,
+          original_date: dto.locals.original_date,
+          new_date: dto.locals.new_date,
+          reason: dto.locals.reason,
+          subject: dto.locals.subject,
+          header: dto.locals.header,
+          type: dto.locals.type,
+          message: dto.locals.message,
+          service_fee: dto.locals.service_fee,
+          total: dto.locals.total,
+        };
+        if (dto.template === "orderPlaced") {
+          await sendEmail({
+            template: "orderPlaced",
+            receiver: dto.receiver,
+            locals,
+          });
+          await sendEmail({
+            template: "orderReceipt",
+            receiver: dto.receiver,
+            locals,
+          });
+        } else {
+          await sendEmail({
+            template: dto.template,
+            receiver: dto.receiver,
+            locals,
+          });
+        }
+        channel.ack(msg!);
       }
     );
   } catch (error) {
